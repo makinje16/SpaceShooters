3308f91cad343f5aa6299f7717401fc7b146273d
Malcolm Akinje <malcolm.akinje@gmail.com>
2017-04-05 16:03:30 -0400

    Starting Up

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioManager : MonoBehaviour {
+
+	public AudioSource gunShot;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public void playGunShot(){
+		gunShot.Play ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
