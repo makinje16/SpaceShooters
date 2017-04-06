762d4a953326ab8d553288145dfcf1a033193ad7
Malcolm Akinje <malcolm.akinje@gmail.com>
2017-04-06 14:57:13 -0400

    The Background music now stays on during all game screens

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,21 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using UnityEngine.SceneManagement;
 public class AudioManager : MonoBehaviour {
 
-	public AudioSource gunShot;
-
+	public AudioSource audio;
+	bool change = false;
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 
 	public void playGunShot(){
-		gunShot.Play ();
+		print ("GunShot");
+		audio.Play();
+		change = true;
 	}
 
+	void CheckChange(){
+		if (!audio.isPlaying && change == true) {
+			SceneManager.LoadScene ("Game");
+		} else {
+			return;
+		}
+	}
 	// Update is called once per frame
 	void Update () {
-	
+		CheckChange ();
 	}
 }
